a6496765467026e4ab17f9ae54d15fd1c32a37e2
Pablo Emilio <101428710+PABLOEMILIO01@users.noreply.github.com>
2023-09-13 19:26:47 -0300

    Add files via upload

---
+++ 05/script.js
@@ -0,0 +1,24 @@
+const loadingText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+
+var load = 0
+var int = setInterval(blurring, 30)
+
+function blurring () {
+    load++;
+    if(load> 99){
+        clearInterval(int);
+    }
+
+    loadingText.innerHTML= `${load}%`;
+    loadingText.style.opacity=scale(load, 0, 100, 10, 0);
+    bg.style.filter=`blur(${scale(load, 0, 100, 20, 0)}px)`
+
+
+
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
